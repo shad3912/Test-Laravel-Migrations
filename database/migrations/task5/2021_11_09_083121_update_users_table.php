9f135d8f2831a90a0fcb2bacfb131090a53455e0
Shad <a.matrai95@gmail.com>
2023-08-06 10:31:04 +0200

    All passed

--- database/migrations/task5/2021_11_09_083121_update_users_table.php
+++ database/migrations/task5/2021_11_09_083121_update_users_table.php
@@ -15,7 +15,12 @@ class UpdateUsersTable extends Migration
     {
         // TASK: add an if-statement in this file to NOT add column if it already exists
         Schema::table('users', function (Blueprint $table) {
-            $table->string('name');
+            if (Schema::hasColumn('users','name')) {
+
+            }
+            else{
+                $table->string('name');
+            }
         });
     }
 
